b181396bf34c1fa55b69e06811be463334612a21
se0hyun <be8shyn@gmail.com>
2026-04-26 23:18:20 +0900

    feat: 유저 레포지토리 추가

---
+++ creator-settlement-pjt/src/main/java/com/seohyun/creator_settlement_pjt/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.seohyun.creator_settlement_pjt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.seohyun.creator_settlement_pjt.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
