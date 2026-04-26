c06e4dd7cc1af565e46aa29585569467c75d7181
se0hyun <be8shyn@gmail.com>
2026-04-26 23:19:13 +0900

    feat: 강의 레포지토리 추가

---
+++ creator-settlement-pjt/src/main/java/com/seohyun/creator_settlement_pjt/repository/CourseRepository.java
@@ -0,0 +1,8 @@
+package com.seohyun.creator_settlement_pjt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.seohyun.creator_settlement_pjt.entity.Course;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+}
